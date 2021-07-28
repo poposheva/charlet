7399c2355a2ff09c22e43438ae1bfbec164a2fa6
poposheva <59781920+poposheva@users.noreply.github.com>
2021-07-28 19:39:10 +0900

    [Refactoring] Cross-site scripting measures

--- main/ui/searchbox.php
+++ main/ui/searchbox.php
@@ -3,7 +3,7 @@
         $screen = new SingleScreen();
 
         $screen->Assign("nowurl",$config["NowURL"]);
-        $screen->Assign("searchword",$option["Word"]);
+        $screen->Assign("searchword",htmlspecialchars($option["Word"]));
 
         return $screen->Fetch("ui/searchbox.tpl");
     }
